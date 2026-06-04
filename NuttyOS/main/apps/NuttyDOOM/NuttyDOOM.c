2ce5b2ba6ceb0d1d3bd65ed3633ea6b6e8f241c7
Wessell <git@wesl.cc>
2026-06-04 19:55:56 +0800

    feat: create initial NuttyDOOM app

---
+++ NuttyOS/main/apps/NuttyDOOM/NuttyDOOM.c
@@ -0,0 +1,15 @@
+#include "NuttyDOOM.h"
+
+
+
+static void nutty_main(void) {
+
+
+
+}
+
+NuttyAppDefinition NuttyDOOM = {
+    .appName = "NuttyDOOM",
+    .appMainEntry = nutty_main,
+    .appHidden = false
+};
